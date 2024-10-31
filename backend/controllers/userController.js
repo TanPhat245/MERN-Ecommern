443440a5e5ba90e6b59fc746c606900da04887b2
TanPhat245 <phatt4071@gmail.com>
2024-10-31 09:13:13 +0700

    add product backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,25 @@ const createToken = (id) => {
 }
 //login
 const loginUser = async (req,res) => {
+    try {
+        const {email,password} = req.body;
+        const user = await userModel.findOne({email});
+        if (!user) {
+            return res.json({success:false, message:"Tài khoản đã tồn tại"})
+        }
 
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            const token = createToken(user._id)
+            res.json({success:true,token})
+        }
+        else {
+            res.json({success:false, message: 'Sai mật khẩu'})
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 }
 
 //dang ky
@@ -45,7 +63,7 @@ const registerUser = async (req,res) => {
 
 //login cho et min
 const adminLogin = async (req,res) => {
-
+    
 }
 
 export { loginUser, registerUser,adminLogin }
